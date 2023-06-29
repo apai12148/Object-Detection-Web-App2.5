ed70207f97289facb8c3374cd0fc86ad259736b4
apai12148 <97473664+apai12148@users.noreply.github.com>
2023-06-29 18:29:57 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function bedroom(){
+    window.location = "bedroom.html"
+}
+
+function kitchen(){
+    window.location = "kitchen.html"
+}
+
+function living_room(){
+    window.location = "living_room.html"
+}
+
+function music_room(){
+    window.location = "music_room.html"
+}
+
+function office(){
+    window.location = "office.html"
+}
